c013b2b2fc0ae7bf70f11e62f1b7211e7fcf8db5
andrew-merz <merz.school@gmail.com>
2022-04-07 16:55:08 -0400

    afternoon commit- figuring out some javascript

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const spaces = document.querySelectorAll(".game-board");
-
+const button1 = document.querySelector(".btn1");
+let currentPlayer = 1;
 for (let i = 0; i < spaces.length; i++) {
   spaces[i].onclick = () => {
-    console.log("Click Works");
+    //console.log("Click Works");
+    // spaces[i + 7] because it needs to check if the div below it is taken
+    if (currentPlayer == 1) {
+      spaces[i].classList.add("player1");
+    }
   };
 }
