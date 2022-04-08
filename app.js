90fd5bbbb7c6a4f618236d838c38bdb788a48b34
andrew-merz <merz.school@gmail.com>
2022-04-07 22:33:54 -0400

    Added alternating chip color function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const spaces = document.querySelectorAll(".game-board");
+const spaces = document.querySelectorAll(".game-board div");
 const button1 = document.querySelector(".btn1");
+const column1 = spaces[(0, 7, 21, 28, 35)];
+
 let currentPlayer = 1;
+
 for (let i = 0; i < spaces.length; i++) {
   spaces[i].onclick = () => {
-    //console.log("Click Works");
-    // spaces[i + 7] because it needs to check if the div below it is taken
     if (currentPlayer == 1) {
       spaces[i].classList.add("player1");
+      currentPlayer = 2;
+    } else if (currentPlayer == 2) {
+      spaces[i].classList.add("player2");
+      currentPlayer = 1;
     }
   };
 }
