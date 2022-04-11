abdc1145e2d9cef08643399243dcd4b92ffcf7e5
andrew-merz <merz.school@gmail.com>
2022-04-11 10:49:56 -0400

    Winning Conditions set and work

--- app.js
+++ app.js
@@ -63,12 +63,111 @@ const column7 = [
   spaces[34],
   spaces[41],
 ];
+const winningCombos = [
+  [0, 1, 2, 3],
+  [41, 40, 39, 38],
+  [7, 8, 9, 10],
+  [34, 33, 32, 31],
+  [14, 15, 16, 17],
+  [27, 26, 25, 24],
+  [21, 22, 23, 24],
+  [20, 19, 18, 17],
+  [28, 29, 30, 31],
+  [13, 12, 11, 10],
+  [35, 36, 37, 38],
+  [6, 5, 4, 3],
+  [0, 7, 14, 21],
+  [41, 34, 27, 20],
+  [1, 8, 15, 22],
+  [40, 33, 26, 19],
+  [2, 9, 16, 23],
+  [39, 32, 25, 18],
+  [3, 10, 17, 24],
+  [38, 31, 24, 17],
+  [4, 11, 18, 25],
+  [37, 30, 23, 16],
+  [5, 12, 19, 26],
+  [36, 29, 22, 15],
+  [6, 13, 20, 27],
+  [35, 28, 21, 14],
+  [0, 8, 16, 24],
+  [41, 33, 25, 17],
+  [7, 15, 23, 31],
+  [34, 26, 18, 10],
+  [14, 22, 30, 38],
+  [27, 19, 11, 3],
+  [35, 29, 23, 17],
+  [6, 12, 18, 24],
+  [28, 22, 16, 10],
+  [13, 19, 25, 31],
+  [21, 15, 9, 3],
+  [20, 26, 32, 38],
+  [36, 30, 24, 18],
+  [5, 11, 17, 23],
+  [37, 31, 25, 19],
+  [4, 10, 16, 22],
+  [2, 10, 18, 26],
+  [39, 31, 23, 15],
+  [1, 9, 17, 25],
+  [40, 32, 24, 16],
+  [9, 17, 25, 33],
+  [8, 16, 24, 32],
+  [11, 17, 23, 29],
+  [12, 18, 24, 30],
+  [1, 2, 3, 4],
+  [5, 4, 3, 2],
+  [8, 9, 10, 11],
+  [12, 11, 10, 9],
+  [15, 16, 17, 18],
+  [19, 18, 17, 16],
+  [22, 23, 24, 25],
+  [26, 25, 24, 23],
+  [29, 30, 31, 32],
+  [33, 32, 31, 30],
+  [36, 37, 38, 39],
+  [40, 39, 38, 37],
+  [7, 14, 21, 28],
+  [8, 15, 22, 29],
+  [9, 16, 23, 30],
+  [10, 17, 24, 31],
+  [11, 18, 25, 32],
+  [12, 19, 26, 33],
+  [13, 20, 27, 34],
+];
 
 let currentPlayer = 1;
-
+let activeGame = true;
 const buttons = [button1, button2, button3, button4, button5, button6, button7];
 const columns = [column1, column2, column3, column4, column5, column6, column7];
 
+// Based on the tic tac toe winning conditins, for every space in the winning array , the function will check if there is a winning combination
+function checkWin() {
+  for (let i = 0; i < winningCombos.length; i++) {
+    const space1 = spaces[winningCombos[i][0]];
+    const space2 = spaces[winningCombos[i][1]];
+    const space3 = spaces[winningCombos[i][2]];
+    const space4 = spaces[winningCombos[i][3]];
+
+    if (
+      space1.classList.contains("player1") &&
+      space2.classList.contains("player1") &&
+      space3.classList.contains("player1") &&
+      space4.classList.contains("player1")
+    ) {
+      displayPlayer.innerHTML = "<h3>Player 1 Wins!!!</h3>";
+      activeGame === false;
+    }
+    if (
+      space1.classList.contains("player2") &&
+      space2.classList.contains("player2") &&
+      space3.classList.contains("player2") &&
+      space4.classList.contains("player2")
+    ) {
+      displayPlayer.innerHTML = "<h3>Player 2 Wins!!!</h3>";
+      activeGame === false;
+    }
+  }
+}
 // for (let i = 0; i < buttons.length; i++) {
 //   for (let j = 0; j < columns.length; j++) {
 //     buttons[i].onclick = () => {
@@ -87,24 +186,57 @@ const columns = [column1, column2, column3, column4, column5, column6, column7];
 //   }
 // }
 
-for (let i = 0; i < spaces.length; i++) {
-  spaces[i].onclick = () => {
-    //if the square below your current square is taken, you can go ontop of it
-    if (
-      spaces[i + 7].classList.contains("taken") &&
-      !spaces[i].classList.contains("taken")
-    ) {
-      if (currentPlayer == 1) {
-        spaces[i].classList.add("taken");
-        spaces[i].classList.add("player1");
-        currentPlayer = 2;
-        displayPlayer.innerHTML = "<h3>Current Player: Player 2</h3>";
-      } else if (currentPlayer == 2) {
-        spaces[i].classList.add("taken");
-        spaces[i].classList.add("player2");
-        currentPlayer = 1;
-        displayPlayer.innerHTML = "<h3>Current Player: Player 1</h3>";
-      }
-    } else alert("cant go here");
-  };
+// for (let i = 0; i < spaces.length; i++) {
+//   spaces[i].onclick = () => {
+//     //if the square below your current square is taken, you can go ontop of it
+//     if (
+//       spaces[i + 7].classList.contains("taken") &&
+//       !spaces[i].classList.contains("taken") &&
+//       activeGame === true
+//     ) {
+//       if (currentPlayer == 1) {
+//         console.log("player one works");
+//         spaces[i].classList.add("taken");
+//         spaces[i].classList.add("player1");
+//         currentPlayer = 2;
+//         displayPlayer.innerHTML = "<h3>Current Player: Player 2</h3>";
+//       } else if (currentPlayer == 2) {
+//         console.log("player two works");
+//         spaces[i].classList.add("taken");
+//         spaces[i].classList.add("player2");
+//         currentPlayer = 1;
+//         displayPlayer.innerHTML = "<h3>Current Player: Player 1</h3>";
+//       }
+//     } else alert("cant go here");
+//     checkWin();
+//   };
+// }
+for (let i = 0; i < buttons.length; i++) {
+  for (let j = 0; j < columns.length; j++) {
+    for (let k = 0; k < spaces.length; k++) {
+      buttons[i].onclick = () => {
+        //if the square below your current square is taken, you can go ontop of it
+        if (
+          spaces[k + 7].classList.contains("taken") &&
+          !spaces[k].classList.contains("taken") &&
+          activeGame === true
+        ) {
+          if (currentPlayer == 1) {
+            console.log("player one works");
+            spaces[k].classList.add("taken");
+            spaces[k].classList.add("player1");
+            currentPlayer = 2;
+            displayPlayer.innerHTML = "<h3>Current Player: Player 2</h3>";
+          } else if (currentPlayer == 2) {
+            console.log("player two works");
+            spaces[k].classList.add("taken");
+            spaces[k].classList.add("player2");
+            currentPlayer = 1;
+            displayPlayer.innerHTML = "<h3>Current Player: Player 1</h3>";
+          }
+        } else alert("cant go here");
+        checkWin();
+      };
+    }
+  }
 }
